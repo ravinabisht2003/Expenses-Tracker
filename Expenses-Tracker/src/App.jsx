d2c51ee547cb77e1893061942c73bd0de13cd4a3
ravinabisht2003 <ravinabisht2003@gmail.com>
2026-06-21 11:14:44 +0530

    feat - adding Navbar

--- Expenses-Tracker/src/App.jsx
+++ Expenses-Tracker/src/App.jsx
@@ -3,7 +3,7 @@ import Nav_bar from './Nav_bar/Nav_bar'
 
 export default function App() {
   return (
-    <div>
+    <div className='m-5 p-0'>
       <Nav_bar/>
     </div>
   )
--- Expenses-Tracker/src/Nav_bar/Nav_bar.jsx
+++ Expenses-Tracker/src/Nav_bar/Nav_bar.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React from "react";
+import expenses_logo from "../assets/expenses_logo.jpg";
+
+import {
+  LayoutDashboard,
+  ArrowLeftRight,
+  Wallet,
+  Goal,
+  Settings,
+  BadgeDollarSign,
+  ChartNoAxesCombined,
+} from "lucide-react";
 
 export default function Nav_bar() {
+  const navbarIcons = [
+    {
+      icon: LayoutDashboard,
+      intro: "Dashboard",
+    },
+    {
+      icon: ArrowLeftRight,
+      intro: "Transcations",
+    },
+    {
+      icon: Wallet,
+      intro: "Wallet",
+    },
+    {
+      icon: Goal,
+      intro: "Goal",
+    },
+    {
+      icon: BadgeDollarSign,
+      intro: "Budget",
+    },
+    {
+      icon: ChartNoAxesCombined,
+      intro: "Analytics",
+    },
+
+    {
+      icon: Settings,
+      intro: "Settings",
+    },
+    
+  ];
+
   return (
-    <div>
-      <h1 className='mb-7 text-5xl font-bold'></h1>
-    </div>
-  )
+    <nav className="w-64 min-h-screen bg-purple-600 shadow-lg rounded-3xl">
+      {/* Logo Section */}
+      <div className="flex items-center gap-4 px-4 py-6">
+        <img
+          src={expenses_logo}
+          alt="Logo"
+          className="h-20 w-20 rounded-full object-cover"
+        />
+
+        <h1 className="text-xl text-amber-50 font-bold">
+          Expense Tracker
+        </h1>
+      </div>
+
+      {/* Menu Items */}
+      <div className="flex flex-col gap-2 px-3 mt-4">
+        {navbarIcons.map((item, index) => {
+          const Icon = item.icon;
+
+          return (
+      
+            <div
+              key={index}
+              className="flex items-center gap-8 pl-6 pb-3 pt-3 mt-1 rounded-lg cursor-pointer hover:bg-purple-500 transition-all duration-300 hover:translate-x-4"
+            >
+              <div><Icon className="w-6 h-6 text-white" /></div>
+              <div><span className="font-medium text-white">{item.intro}</span></div>
+            </div>
+            
+          );
+        })}
+      </div>
+    </nav>
+  );
 }
