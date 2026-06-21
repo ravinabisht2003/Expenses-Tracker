889670ebcaf5d6f6d528970c5350d71a1fe68e45
ravinabisht2003 <ravinabisht2003@gmail.com>
2026-06-21 15:20:55 +0530

    Changes - navbar changes

--- Expenses-Tracker/src/Nav_bar/Nav_bar.jsx
+++ Expenses-Tracker/src/Nav_bar/Nav_bar.jsx
@@ -9,6 +9,8 @@ import {
   Settings,
   BadgeDollarSign,
   ChartNoAxesCombined,
+  CircleQuestionMark,
+  LogOut,
 } from "lucide-react";
 
 export default function Nav_bar() {
@@ -48,20 +50,20 @@ export default function Nav_bar() {
   return (
     <nav className="w-64 min-h-screen bg-purple-600 shadow-lg rounded-3xl">
       {/* Logo Section */}
-      <div className="flex items-center gap-4 px-4 py-6">
+      <div className="flex items-center gap-5 px-4 py-6  rounded-t-3xl mb-5 bg-purple-500">
         <img
           src={expenses_logo}
           alt="Logo"
-          className="h-20 w-20 rounded-full object-cover"
+          className="h-18 w-18 rounded-full object-cover"
         />
 
-        <h1 className="text-xl text-amber-50 font-bold">
-          Expense Tracker
+        <h1 className="text-l  text-amber-50 font-bold">
+          Money Tracker
         </h1>
       </div>
 
       {/* Menu Items */}
-      <div className="flex flex-col gap-2 px-3 mt-4">
+      <div className="flex flex-col gap-2 px-3">
         {navbarIcons.map((item, index) => {
           const Icon = item.icon;
 
@@ -69,15 +71,32 @@ export default function Nav_bar() {
       
             <div
               key={index}
-              className="flex items-center gap-8 pl-6 pb-3 pt-3 mt-1 rounded-lg cursor-pointer hover:bg-purple-500 transition-all duration-300 hover:translate-x-4"
+              className="flex items-center gap-9 pl-6 pb-3 pt-3  rounded-lg cursor-pointer hover:bg-blue-800 transition-all duration-300 hover:translate-x-4"
             >
               <div><Icon className="w-6 h-6 text-white" /></div>
               <div><span className="font-medium text-white">{item.intro}</span></div>
             </div>
             
           );
+          
         })}
+
       </div>
+    <div className="flex flex-col gap-2 px-3 mt-15 pb-25" >
+      <div className="items-center  ">
+      <div className="flex gap-9 pl-6 pb-3 pt-3 text-white rounded-lg cursor-pointer hover:bg-blue-800 transition-all duration-300 hover:translate-x-4">
+          <span><CircleQuestionMark /></span>
+          <span>Help</span>
+      </div>
+      <div className="flex gap-9 pl-6 pb-3 pt-3 text-white rounded-lg cursor-pointer hover:bg-blue-800 transition-all duration-300 hover:translate-x-4">
+          <span><LogOut /></span>
+          <span>Logout</span>
+      </div>
+    </div>
+    </div>
+
+        
+      
     </nav>
   );
 }
