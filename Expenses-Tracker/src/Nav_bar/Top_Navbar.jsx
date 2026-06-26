d2a37cfd8ef462e774a6c369733fa20acbe1c76f
ravinabisht2003 <ravinabisht2003@gmail.com>
2026-06-26 18:22:14 +0530

    feat - adding changes in navbar

--- Expenses-Tracker/src/Nav_bar/Top_Navbar.jsx
+++ Expenses-Tracker/src/Nav_bar/Top_Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Search,Bell,CalendarDays} from 'lucide-react'
+import {Search,Bell,CalendarDays,LayoutGrid,Plus} from 'lucide-react'
 import expenses_logo from "../assets/expenses_logo.jpg";
 
 export default function Top_Navbar() {
@@ -11,8 +11,8 @@ export default function Top_Navbar() {
       <h6 className='text-gray-300 font-bold text-xs mt-1'>It is the best time to manage your finances</h6>
     </div>
     
-    <div className='flex items-center gap-6'>
-      <div className='flex gap-6 items-center '>
+    <div className='flex items-center gap-4'>
+      <div className='flex gap-2 items-center '>
       <span className='border-2 border-gray-400 p-1 rounded-full'><Search/></span>
       <span className='border-2 border-gray-400 p-1 rounded-full'><Bell/></span>
       </div>
@@ -27,14 +27,20 @@ export default function Top_Navbar() {
       </div>
     </div>
     </div>
-    <div className='mt-10 flex items-center justify-between'>
+    <div className='mt-5 flex items-center justify-between'>
       <div className='flex items-center gap-2 '>
       <span className='border-2 p-2 rounded-full border-gray-300'><CalendarDays size={20}/></span>
       <div className='border-2 border-gray-400 p-2 rounded-full text-xs'>This month</div>
       </div>
-      <div>
-        <span></span>
-        <span>Manage widgets</span>
+      <div className='text-xs flex justify-center items-center gap-8'>
+        <div className='flex items-center gap-2 border-2 border-gray-300 p-3 pt-1 pb-1 rounded-full' >
+        <span><LayoutGrid /></span>
+        <span className=''>Manage widgets</span>
+        </div>
+        <div className='flex justify-center items-center text-white border-2 border-gray-300  p-3 pt-1 pb-1 gap-2 rounded-full bg-blue-500'>
+          <span><Plus /></span>
+          <span>Add new widget</span>
+        </div>
       </div>
     </div> 
     </div>
