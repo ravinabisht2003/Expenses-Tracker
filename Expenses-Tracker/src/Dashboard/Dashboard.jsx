3474a38f2efc65fecf8dce7b9b5e3bd19f60bd7a
ravinabisht2003 <ravinabisht2003@gmail.com>
2026-06-28 07:58:03 +0530

    feat - adding dashbord one part

--- Expenses-Tracker/src/Dashboard/Dashboard.jsx
+++ Expenses-Tracker/src/Dashboard/Dashboard.jsx
@@ -1,58 +1,64 @@
-import React from 'react'
-import {Icon,ArrowUpRight,ArrowUp} from 'lucide-react'
+import React from "react";
+import { Icon, ArrowUpRight, ArrowUp } from "lucide-react";
 
 export default function Dashboard() {
-    const Dashboard = [
+  const Dashboard = [
     {
       title: "Total balance",
       amount: 15700,
-      percentage:12.1,
+      percentage: 12.1,
     },
     {
       title: "Income",
       amount: 8500,
-      percentage:6.3,
-
+      percentage: 6.3,
     },
     {
       title: "Expense",
       amount: 6222,
-      percentage:2.4,
+      percentage: 2.4,
     },
     {
       title: "Total savings",
       amount: 32913,
-      percentage:12.1,
-    },    
+      percentage: 12.1,
+    },
   ];
   return (
-    
-    <div className="flex  flex-row mt-6 gap-2">
-        {Dashboard.map((item, index) => {
-          const Icon = item.icon;
+    <div className="flex  flex-row mt-6 gap-6">
+      {Dashboard.map((item, index) => {
+        const Icon = item.icon;
 
-          return (
-      
-            <div
-              key={index}
-              className=" w-full pt-3 pb-3 p-5 rounded-lg cursor-pointer border-2 border-gray-300"
-            >
-            <div className='flex justify-between w-full items-center gap-25'>
-              <div className='font-bold'>{item.title}</div>
-              <div><span className="font-medium"><ArrowUpRight /></span></div>
+        return (
+          <div
+            key={index}
+            className="w-full h-40 pt-1 pl-3 pb-3  rounded-4xl cursor-pointer border-2 border-gray-300"
+          >
+            <div className="flex justify-between items-center">
+              <div className="font-bold">{item.title}</div>
+              <div className="flex">
+                <span className="font-medium border-2 border-gray-400 p-2 rounded-full ">
+                  <ArrowUpRight size={20} />
+                </span>
+              </div>
             </div>
-                <div className='font-bold text-2xl mt-4'>${item.amount}</div>
-                <div className='flex items-center gap-2 ' >
-                    
-                <div className='bg-green-400 w-fit mt-2 rounded-full p-1 text-xs'><ArrowUp />{item.percentage}%</div>
-                <div className=' text-gray-400 text-xs font-bold'>vs last month</div>
+            <div className="font-bold text-2xl mt-5">${item.amount} <span className="text-gray-400">.{item.amount.toFixed(2).split(".")[1]}</span></div>
+            <div className="flex items-center mt-3 gap-1">
+              
+                <div className=" p-1 gap-1 w-fit rounded-full bg-green-300 text-green-500 text-xs flex items-center">
+                  <div>
+                    <ArrowUp size={18} />
+                  </div>
+                  <div>{item.percentage}%</div>
                 </div>
+                <div className=" text-gray-400 text-xs font-bold">
+                  vs last month
+                
+              </div>
             </div>
-            
-          );
-          
-        })}
+          </div>
+        );
+      })}
     </div>
-
-  )
+  );
 }
